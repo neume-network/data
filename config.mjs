655d96ae922f4a6ba1eec538c802bb9616d40cf6
il3ven <vaibhavchanana@yahoo.co.in>
2022-08-02 02:09:21 +0530

    increase timeout

--- config.mjs
+++ config.mjs
@@ -3,17 +3,17 @@ import { env } from "process";
 export default {
   endpoints: {
     [env.RPC_HTTP_HOST]: {
-      timeout: 4000,
+      timeout: 10000,
       requestsPerUnit: 12,
       unit: "second",
     },
     "https://metadata.sound.xyz": {
-      timeout: 4000,
+      timeout: 10000,
       requestsPerUnit: 6,
       unit: "second",
     },
     "https://neume.infura-ipfs.io": {
-      timeout: 4000,
+      timeout: 10000,
       requestsPerUnit: 100,
       unit: "second",
     },
