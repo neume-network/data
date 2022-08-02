39b2a059d9581a8a24e472802867a63af3aa4237
il3ven <vaibhavchanana@yahoo.co.in>
2022-08-03 02:47:21 +0530

    remove timeout, run full crawl

--- config.mjs
+++ config.mjs
@@ -3,18 +3,18 @@ import { env } from "process";
 export default {
   endpoints: {
     [env.RPC_HTTP_HOST]: {
-      timeout: 10000,
-      requestsPerUnit: 8,
+      // timeout: 10000,
+      requestsPerUnit: 10,
       unit: "second",
     },
     "https://metadata.sound.xyz": {
-      timeout: 10000,
+      // timeout: 10000,
       requestsPerUnit: 6,
       unit: "second",
     },
     "https://neume.infura-ipfs.io": {
-      timeout: 10000,
-      requestsPerUnit: 100,
+      // timeout: 10000,
+      requestsPerUnit: 80,
       unit: "second",
     },
   },
--- steps/0.mjs
+++ steps/0.mjs
@@ -2,9 +2,7 @@ export default [
   [
     {
       name: "web3subgraph",
-      extractor: {
-        args: ["0xabefbc9fd2f806065b4f3c237d4b59d9a97bcac7/8954"],
-      },
+      extractor: {},
       transformer: {},
     },
   ],
