bff9664c48e9034891e99aeeb21a9ce3f7cb92bc
il3ven <vaibhavchanana@yahoo.co.in>
2022-08-04 00:11:33 +0530

    update timeout, frequency and concurrency

--- config.mjs
+++ config.mjs
@@ -3,17 +3,17 @@ import { env } from "process";
 export default {
   endpoints: {
     [env.RPC_HTTP_HOST]: {
-      // timeout: 10000,
-      requestsPerUnit: 10,
+      timeout: 25000,
+      requestsPerUnit: 25,
       unit: "second",
     },
     "https://metadata.sound.xyz": {
-      // timeout: 10000,
-      requestsPerUnit: 6,
+      timeout: 25000,
+      requestsPerUnit: 10,
       unit: "second",
     },
     "https://neume.infura-ipfs.io": {
-      // timeout: 10000,
+      timeout: 25000,
       requestsPerUnit: 80,
       unit: "second",
     },
