51de9b5cfcec34a31f17afa238fad0b974916a67
Tim Daubenschütz <tim@daubenschuetz.de>
2022-08-31 17:23:22 +0200

    bla

--- steps/1.mjs
+++ steps/1.mjs
@@ -1,13 +1,8 @@
-import { resolve } from "path";
-import { env } from "process";
-
 export default [
   [
     {
       name: "logs-to-subgraph",
-      transformer: {
-        args: [resolve(env.DATA_DIR, "call-block-logs-transformation")]
-      },
+      transformer: {},
     },
   ],
 ];
