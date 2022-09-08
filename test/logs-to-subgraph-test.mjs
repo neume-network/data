a1b7de53e8e1e00434bcaee4ae908e6eff309fbe
Tim Daubenschütz <tim@daubenschuetz.de>
2022-09-08 09:40:47 +0200

    Test logs-to-subgraph file on duplicates

---
+++ test/logs-to-subgraph-test.mjs
@@ -0,0 +1,31 @@
+// @format
+import { resolve } from "path";
+
+import test from "ava";
+
+import { slurpadd } from "../src/utils.mjs";
+
+const fileName = "logs-to-subgraph-transformation";
+test(`if ${fileName} file contains duplicates`, async (t) => {
+  const list = await slurpadd(resolve("results", fileName));
+  const ids = list.map((log) => `${log.address}/${log.tokenId}`);
+  let map = {};
+  for (const id of ids) {
+    if (map[id]) {
+      map[id] += 1;
+    } else {
+      map[id] = 1;
+    }
+  }
+
+  let poisoned = false;
+  for (const [key, value] of Object.entries(map)) {
+    if (value !== 1) {
+      poisoned = true;
+      console.log(
+        `For key "${key}" occurrence in ${fileName} is ${value} (max: 1)`
+      );
+    }
+  }
+  t.false(poisoned);
+});
